43a6fd61da0efcff8d9c88b499de6a9b67f2c864
student <snyphar.ex@gmail.com>
2019-02-06 11:23:06 +0600

    My First Project

---
+++ app/src/main/java/com/example/snyph/assignment01/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.snyph.assignment01;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private int notificationId = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.add_button).setOnClickListener(this);
+        findViewById(R.id.cancel_button).setOnClickListener(this);
+    }
+    public void onClick(View view){
+
+        EditText editText = findViewById(R.id.edit_txt);
+        TimePicker timePicker = findViewById(R.id.time_Picker);
+
+        // Set notificationId & text.
+        Intent intent = new Intent(MainActivity.this, AlarmReceiver.class);
+        intent.putExtra("notificationId", notificationId);
+        intent.putExtra("todo", editText.getText().toString());
+
+        // getBroadcast(context, requestCode, intent, flags)
+        PendingIntent alarmIntent = PendingIntent.getBroadcast(MainActivity.this, 0,
+                intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        AlarmManager alarm = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+        switch (view.getId()) {
+            case R.id.add_button:
+                int hour = timePicker.getCurrentHour();
+                int minute = timePicker.getCurrentMinute();
+
+                // Create time.
+                Calendar startTime = Calendar.getInstance();
+                startTime.set(Calendar.HOUR_OF_DAY, hour);
+                startTime.set(Calendar.MINUTE, minute);
+                startTime.set(Calendar.SECOND, 0);
+                long alarmStartTime = startTime.getTimeInMillis();
+
+                // Set alarm.
+                // set(type, milliseconds, intent)
+                alarm.set(AlarmManager.RTC_WAKEUP, alarmStartTime, alarmIntent);
+
+                Toast.makeText(this, "Done!", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.cancel_button:
+                alarm.cancel(alarmIntent);
+                Toast.makeText(this, "Canceled.", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+    }
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+
+        // Check which checkbox was clicked
+
+
+
+        }
+
+
+    }
+
